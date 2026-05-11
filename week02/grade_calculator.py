58dbcad729146b0912b72bed9c859a3c642d25c9
User <tihas75@mail.ru>
2026-05-11 23:07:26 +0300

    Добавил grade_calculator

---
+++ week02/grade_calculator.py
@@ -0,0 +1,17 @@
+# Задача: Пользователь вводит баллы (0 - 100). Программа выводит букву оценки:
+# 90 - 100 -> A, 80 - 89 -> B, 70 - 79 -> C, 60 - 69 -> D, < 60 -> F
+
+score = int(input("Введите баллы (0 - 100): "))
+
+if score >= 90:
+    grade = "A"
+elif score >= 80:
+    grade = "B"
+elif score >= 70:
+    grade = "C"
+elif score >= 60:
+    grade = "D"
+else:
+    grade = "F"
+
+print(f"Ваша оценка: {grade}")
