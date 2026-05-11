f2af30d40715af74e5899cced7ee784e88f8fbba
User <tihas75@mail.ru>
2026-05-11 19:53:40 +0300

    Добавил visiting_card

---
+++ week01/visiting_card.py
@@ -0,0 +1,14 @@
+# Создаем переменные
+my_name = "Александр Тихонов"
+my_profession = "Будущий AI-инженер"
+my_experience = "Новичок"
+my_goal = "Создать умных помощников"
+
+# Выводим визитку
+print("МОЯ ВИЗИТКА")
+print("-" * 30)
+print("Имя:", my_name)
+print("Профессия:", my_profession)
+print("Опыт:", my_experience)
+print("Цель:", my_goal)
+print("-" * 30)
